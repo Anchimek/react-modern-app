51b46c29aaff3dfca33360b6c1e96df8002fed97
Kuba <jakub.anchimowicz@gmail.com>
2023-05-07 09:30:24 +0200

    update

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -4,15 +4,33 @@ import { arrowUp } from '../assets'
 
 const GetStarted = () => (
     <div className={`
-      bg-blue-gradient rounded-full w-[140px] h-[140px] 
-      ${styles.flexCenter} p-[2px] cursor-pointer`
-    }>
-      <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+      bg-blue-gradient 
+      rounded-full 
+      w-[140px] h-[140px] 
+      ${styles.flexCenter} 
+      p-[2px] cursor-pointer feature-card`}
+    >
+      <div className={`
+          ${styles.flexCenter} 
+          flex-col 
+          bg-primary
+          w-[100%] h-[100%] 
+          rounded-full`}
+      >
         <div className={`${styles.flexStart} flex-row`}>
-          <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
+          <p className='
+            font-poppins font-medium 
+            text-[18px] 
+            leading-[23px] 
+            mr-2'
+          >
             <span className='text-gradient'>Get</span>
           </p>
-            <img src={arrowUp} alt='arrow' className='w-[23px] h-[23px] object-contain '/>
+            <img 
+              src={arrowUp} 
+              alt='arrow' 
+              className='w-[23px] h-[23px] object-contain '
+            />
         </div>
         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
             <span className='text-gradient'>Started</span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,12 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <div>
-      <nav className='w-full flex py-6 jusify-between 
-      items-center navbar'>
+      <nav className='
+        w-full 
+        py-6 
+        flex jusify-between items-center  
+        navbar'
+      >
         <img 
           src={logo} 
           alt='hoobank' 
@@ -19,7 +23,12 @@ const Navbar = () => {
           {navLinks.map((nav, index) => (
             <li
               key={nav.id}
-              className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}
+              className={`
+                font-poppins font-normal 
+                cursor-pointer 
+                text-[16px] 
+                ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} 
+                text-white feature-card`}
             > 
               <a href={`#${nav.id}`}>
                 {nav.title}
@@ -36,13 +45,27 @@ const Navbar = () => {
             onClick={() => setToggle( prev => !prev)}
           />
           <div 
-            className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            className={`
+              ${toggle ? 'flex' : 'hidden'} 
+              p-6 
+              bg-black-gradient 
+              absolute 
+              top-20 right-0 
+              mx-4 my-2 
+              min-w-[140px] 
+              rounded-xl 
+              sidebar`}
           >
             <ul className='list-none flex justify-end items-center flex-1 flex-col'>
               {navLinks.map((nav, index) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-normal cursor-pointer text-[16px] ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}
+                  className={`
+                  font-poppins font-normal 
+                  cursor-pointer 
+                  text-[16px] 
+                  ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} 
+                  text-white`}
                 > 
                   <a href={`#${nav.id}`}>
                     {nav.title}
@@ -52,7 +75,6 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-
       </nav>
     </div>
   )
